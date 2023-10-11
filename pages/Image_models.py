6e9ae2cb864cf7eafb8e0fd585b872ac16c69464
Hardik Mittal <mhardik2003@gmail.com>
2023-10-11 13:34:37 +0530

    Added README for Image Models

--- pages/Image_models.py
+++ pages/Image_models.py
@@ -16,7 +16,43 @@ st.set_page_config(page_title="Image Model Library", layout="wide", page_icon="ð
 uploaded_file = st.file_uploader("Choose an image", type=[
     'png', 'jpg', 'jpeg'], accept_multiple_files=False,  help='Upload an image')
 
-st.text("README here")
+readme_content='''
+
+
+## What is this page about?
+This is the page for Image Models.
+
+## What are the models available?
+The following models are available for the following tasks:
+* Image Classification
+    - ALIGN
+    - CLIP
+    - BLIP
+    - BLIPv2
+    - GLIP
+
+* Image Captioning
+    
+
+* Question Answering
+    - BLIPv2
+
+
+# How to use this page?
+* Upload an image
+* Select the task you want to perform on the image
+* Select the model you want to run the image on
+* Enter the prompts if required
+* Click on the run button
+* Wait for the results to load
+
+
+
+
+'''
+if uploaded_file is None:
+    
+    st.code(readme_content, language='markdown')
 
 if uploaded_file is not None:
     left_col, right_col = st.columns(2, gap="large")
